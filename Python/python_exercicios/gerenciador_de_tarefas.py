67a45e9a89a5db7772186d9684ddd6740a75aa94
Vinicyus Eduardo Vicentini Faria <108416319+1vinicyus@users.noreply.github.com>
2026-07-24 11:25:28 -0300

    Add files via upload

--- Python/python_exercicios/gerenciador_de_tarefas.py
+++ Python/python_exercicios/gerenciador_de_tarefas.py
@@ -10,41 +10,80 @@ def adc_tarefas():
         "prioridade": prioridade,
         "status": "pendente" #definido como pendente
     }
+    for ticket in tarefas:
+        if ticket['descrição'] == descricao:
+            print("Essa tarefa já existe")
+            return
+        
     tarefas.append(ticket)
+    print("Tarefa adicionada!")
+
 #--------------------------------------------   
 #3
 def check_tarefa():
-    check = input("Qual Tarefa deseja Concluir?\n")
+    check = input("Qual Tarefa deseja Concluir? (digite a descrição)\n")
 
     for ticket in tarefas:
-        if check == ticket['status']:
-            pass
-    
-
+        if check == ticket['descrição']:
+            ticket['status'] = "Concluída"
+            print("Tarefa Concluída!")
+            return
+        
+    print("Tarefa não encontrada!")
 #--------------------------------------------   
 #4
+def remove_tarefa():
+    remove = input("Qual tarefa deseja remover? (Digite a Descrição)")
+
+    for ticket in tarefas:
+        if remove == ticket['descrição']:
+            tarefas.remove(ticket)
+            print("Tarefa Removida")
+            return
+        
+    print("Tarefa não encontrada!")
 #--------------------------------------------   
 #5
-#--------------------------------------------   
-    num = 0
-    while num != 6:
-        print("=== Gerenciados de Tarefas ===\n")
-        print("1 - Adicionar Tarefas\n")
-        print("2 - Listar Tarefas\n")
-        print("3 - Marcar Tarefa como Concluída\n")
-        print("4 - Remover Tarefa\n")
-        print("5 - Buscar tarefa por Descrição\n")
-        print("6 - Sair")
-        num = int(input("Digite a opção que deseja (1-6): \n"))
-        if num == 1:
-          adc_tarefas()
-    
-        if num == 2:
-            for ticket in tarefas:
-                print(f"Descrição: {ticket['descrição']}")
-                print(f"Prioridade: {ticket['prioridade']}")
-                print(f"Status: {ticket['status']}")
+def busca():
+    buscar = input("Qual tarefa deseja buscar? (Digite a Descrição)")
 
-        if num == 3: 
-          check_tarefa()         
-          
+    for ticket in tarefas:
+        if buscar == ticket['descrição']:
+            print(f"Descrição: {ticket['descrição']}")
+            print(f"Prioridade: {ticket['prioridade']}")
+            print(f"Status: {ticket['status']}")
+    return
+        
+print("Tarefa não encontrada!")
+#--------------------------------------------   
+num = 0
+while num != 6:
+    print("=== Gerenciados de Tarefas ===\n")
+    print("1 - Adicionar Tarefas")
+    print("2 - Listar Tarefas")
+    print("3 - Marcar Tarefa como Concluída")
+    print("4 - Remover Tarefa")
+    print("5 - Buscar tarefa por Descrição")
+    print("6 - Sair")
+    num = int(input("Digite a opção que deseja (1-6):"))
+    if num == 1:
+        adc_tarefas()   
+
+    if num == 2:
+        for ticket in tarefas:
+            print(f"Descrição: {ticket['descrição']}")
+            print(f"Prioridade: {ticket['prioridade']}")
+            print(f"Status: {ticket['status']}")
+        if not tarefas: 
+            print("Nenhuma tarefa Listada!")           
+
+    if num == 3: 
+        check_tarefa()
+
+    if num == 4:
+        remove_tarefa()
+
+    if num == 5:
+        busca()
+
+    if num == 6: print("Programa Encerrado...")      
