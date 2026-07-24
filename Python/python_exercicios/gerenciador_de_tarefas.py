cb0b82d10e90d8decf5282fadeade264b7e5d9da
Vinicyus Eduardo Vicentini Faria <108416319+1vinicyus@users.noreply.github.com>
2026-07-24 11:26:06 -0300

    Add files via upload

--- Python/python_exercicios/gerenciador_de_tarefas.py
+++ Python/python_exercicios/gerenciador_de_tarefas.py
@@ -10,8 +10,8 @@ def adc_tarefas():
         "prioridade": prioridade,
         "status": "pendente" #definido como pendente
     }
-    for ticket in tarefas:
-        if ticket['descrição'] == descricao:
+    for check in tarefas:
+        if check['descrição'] == descricao:
             print("Essa tarefa já existe")
             return
         
@@ -21,7 +21,7 @@ def adc_tarefas():
 #--------------------------------------------   
 #3
 def check_tarefa():
-    check = input("Qual Tarefa deseja Concluir? (digite a descrição)\n")
+    check= input("Qual Tarefa deseja Concluir? (digite a descrição)\n")
 
     for ticket in tarefas:
         if check == ticket['descrição']:
@@ -52,9 +52,8 @@ def busca():
             print(f"Descrição: {ticket['descrição']}")
             print(f"Prioridade: {ticket['prioridade']}")
             print(f"Status: {ticket['status']}")
-    return
-        
-print("Tarefa não encontrada!")
+            return
+    print("Tarefa não encontrada!")
 #--------------------------------------------   
 num = 0
 while num != 6:
@@ -70,10 +69,21 @@ while num != 6:
         adc_tarefas()   
 
     if num == 2:
+        tarefas_con = []
+        tarefas_pend = []
         for ticket in tarefas:
-            print(f"Descrição: {ticket['descrição']}")
-            print(f"Prioridade: {ticket['prioridade']}")
-            print(f"Status: {ticket['status']}")
+            if ticket['status'] == "Concluída":
+                tarefas_con.append(ticket)
+            else: 
+                tarefas_pend.append(ticket)
+           
+        print("=== Tarefas Concluídas ===")
+        for ticket in tarefas_con:
+            print(ticket["descrição"])
+
+        print("\n=== Tarefas Pendentes ===")
+        for ticket in tarefas_pend:
+            print(ticket["descrição"])
         if not tarefas: 
             print("Nenhuma tarefa Listada!")           
 
